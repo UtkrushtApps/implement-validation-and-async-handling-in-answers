440fc57af19e7abbde324fe948ff4ddfc9655992
Naman Muley <naman.g.muley@gmail.com>
2025-12-01 18:42:25 +0530

    Add src/main/java/com/teamboard/user/repository/UserRepository.java

---
+++ src/main/java/com/teamboard/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.teamboard.user.repository;
+
+import com.teamboard.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
