563969e9b4727e16b60704390b80ef797154fb77
Naman Muley <naman.g.muley@gmail.com>
2025-12-01 18:42:32 +0530

    Add src/main/java/com/teamboard/user/exception/ApiError.java

---
+++ src/main/java/com/teamboard/user/exception/ApiError.java
@@ -0,0 +1,17 @@
+package com.teamboard.user.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class ApiError {
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private List<String> messages;
+    private String path;
+}
